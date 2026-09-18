38bd58efd64d66b601cc1ea94fb85d83ec38762b
Richard Johansson <richard.johansson@irf.se>
2026-09-18 11:00:50 +0200

    waspam retrieval first pass work... I think

--- src/krisp/arts/tools.py
+++ src/krisp/arts/tools.py
@@ -1,9 +1,11 @@
 import xarray as xr
-from pyarts import Workspace
+from pyarts.arts import GriddedField3, Tensor3
+import numpy as np
 
 
 def opacity_from_arts(data: xr.Dataset) -> float:
     pass
+
 
 def make_sx_o3(p):
     w_min = 0.1
@@ -12,3 +14,16 @@ def make_sx_o3(p):
     weights = w_min + (1 - w_min) / (1 + np.exp(-k * (np.log10(p0) - np.log10(p))))
     return weights
 
+
+def set_griddedfield3(pressure, latitude, longitude, data):
+    gf = GriddedField3()
+    gf.set_grid_name(0, "Pressure")
+    gf.set_grid_name(1, "Latitude")
+    gf.set_grid_name(2, "Longitude")
+    gf.set_grid(0, pressure)
+    gf.set_grid(1, [latitude])
+    gf.set_grid(2, [longitude])
+
+    data = Tensor3(data[:, np.newaxis, np.newaxis])
+    gf.data = data
+    return gf
---
+++ src/krisp/arts/waspam/retrieve_cwaspam.py
@@ -0,0 +1,284 @@
+from krisp.data.readers import WaspamReader, WaspamData
+from krisp.arts.tools import set_griddedfield3
+from pathlib import Path
+import pyarts
+import numpy as np
+from dataclasses import dataclass
+from datetime import datetime
+import xarray as xr
+
+
+@dataclass
+class Measurement:
+    data: WaspamData
+    lines_cat: str
+    atm_cat: str
+    source: Path
+
+
+def set_defaults(ws, meas: Measurement):
+    # initialise workspace variables for retrieval
+    ws.x = np.array([])
+    ws.yf = np.array([])
+    ws.jacobian = np.array([[]])
+
+    # initialise retrieval framework
+    ws.jacobianInit()
+    ws.retrievalDefInit()
+
+    # %% agendas
+    ws.ppath_agendaSet(option="FollowSensorLosPath")
+    ws.iy_main_agendaSet(option="Emission")
+    ws.surface_rtprop_agendaSet(option="Specular_NoPol_ReflFix_SurfTFromt_surface")
+    ws.ppath_step_agendaSet(option="GeometricPath")
+    ws.iy_space_agendaSet(option="CosmicBackground")
+    ws.iy_surface_agendaSet(option="UseSurfaceRtprop")
+    ws.water_p_eq_agendaSet(option="MK05")
+
+    # %% calculations
+    ws.iy_unit = "RJBT"
+    ws.ppath_lmax = 1e3  # max spacing along path between points
+    ws.ppath_lraytrace = 1e3  # spacing for ray tracing
+    ws.rt_integration_option = "default"
+    ws.nlteOff()
+    ws.refellipsoidEarth(model="Sphere")
+
+    # %% species and line absorption
+    ws.abs_speciesSet(species=["H2O-161"])  # water vapour common isotopologue
+    ws.abs_lines_per_speciesReadSpeciesSplitCatalog(basename=meas.lines_cat)
+    ws.abs_lines_per_speciesCutoff(option="ByLine", value=10e9)
+
+    # %% sensor
+    ws.stokes_dim = 1
+    ws.atmosphere_dim = 1
+
+    # %% grids and planet
+    ws.AtmosphereSet1D()
+    now = datetime.now()
+    print(f"-- {now}: Successfully set defaults in arts")
+
+
+def set_atmosphere(ws, meas: Measurement):
+    ws.xa = meas.data.apriori
+
+    ws.p_grid = meas.data.p
+    ws.lat_grid = np.array([])
+    ws.lon_grid = np.array([])
+    ws.AtmRawRead(basename=meas.atm_cat)
+    ws.AtmFieldsCalc()
+    ws.z_surface = [[16000]]
+    gf_t = set_griddedfield3(
+        pressure=meas.data.p,
+        latitude=0,
+        longitude=0,
+        data=meas.data.temperature,
+    )
+    gf_h2o = set_griddedfield3(
+        pressure=meas.data.p,
+        latitude=0,
+        longitude=0,
+        data=meas.data.h2o,
+    )
+    ws.t_field_raw = gf_t
+    ws.vmr_field_raw.value[0] = gf_h2o
+    ws.AtmFieldsCalc()
+    now = datetime.now()
+    print(f"-- {now}: Successfully set atmosphere in arts")
+
+
+def set_measurement(ws, meas: Measurement):
+    timestamp = meas.data.mid
+    dt = datetime.fromtimestamp(timestamp)
+    time = pyarts.arts.Time(dt)
+    arr_of_time = pyarts.arts.ArrayOfTime(1, time)
+    ws.sensor_time = arr_of_time
+
+    ws.y = meas.data.y + 2.73
+    ws.yf = []
+    ws.y_baseline = ws.y.value * 0
+    ws.f_backend = meas.data.f
+
+    # --- NOTE: HARDCODED!!! ---
+    yerr = np.full_like(meas.data.f, 0.05)
+    ws.covmat_seSet(covmat=pyarts.arts.Sparse(np.diag(yerr)))
+    now = datetime.now()
+    print(f"-- {now}: Successfully set measurements in arts")
+
+
+def set_sensor(ws, meas: Measurement):
+    @pyarts.workspace.arts_agenda(ws=ws, set_agenda=True)
+    def sensor_response_agenda(ws):
+        ws.AntennaOff()
+        ws.sensor_responseInit(sensor_norm=1)
+        ws.sensor_responseBackend(sensor_norm=1)
+
+    n = len(meas.data.f)
+    fe = meas.data.f[-1]
+    fs = meas.data.f[0]
+    res = (fe - fs) / n
+    ws.backend_channel_responseFlat(resolution=res)
+    ws.sensor_response_pol_grid = np.array([0])
+    ws.sensor_response_dlos_grid = [[90, 0]]
+
+    # -- CHECK THIS: Might be to simple
+    ws.f_grid = np.linspace(fs - res, fe + res, n)
+    ws.sensor_pos = [[16000]]
+    ws.sensor_los = [[75]]
+    ws.sensor_response_agenda.value.execute(ws)
+    now = datetime.now()
+    print(f"-- {now}: Successfully set sensor in arts")
+
+
+def set_retrieval_quantities(ws, meas: Measurement):
+    clip_lo = 1e-8
+    clip_hi = 2e-3
+
+    @pyarts.workspace.arts_agenda(ws=ws, set_agenda=True)
+    def inversion_iterate_agenda(ws):
+        ws.Ignore(ws.inversion_iteration_counter)
+        ws.xClip(ijq=0, limit_low=clip_lo, limit_high=clip_hi)
+        ws.x2artsAtmAndSurf()
+        ws.x2artsSensor()
+        ws.yCalc(y=ws.yf)
+        ws.VectorAddElementwise(ws.yf, ws.yf, ws.y_baseline)
+        ws.jacobianAdjustAndTransform()
+
+    # -- HARDCODED xa_sx
+    sx = np.full_like(meas.data.p, 5e-6)
+    ws.retrievalAddAbsSpecies(
+        species="H2O-161",
+        covmat_block=pyarts.arts.Sparse(np.diag(sx)),
+        unit="vmr",
+        g1=ws.p_grid,
+        g2=ws.lat_grid.value,
+        g3=ws.lon_grid.value,
+    )
+
+    # -- Polyfit
+    poly_order = 4
+    poly_var = [1, 0.05, 0.05, 0.05, 0.05]
+    ws.retrievalAddPolyfit(
+        poly_order=poly_order,
+        no_pol_variation=0,
+        no_los_variation=0,
+        sensor_response_pol_grid=ws.sensor_response_pol_grid.value,
+        sensor_response_dlos_grid=ws.sensor_response_dlos_grid.value,
+    )
+
+    for v in poly_var:
+        ws.covmat_sxAddBlock(block=np.diag([v]))
+
+    # -- Frequency shift
+    fs_df = 1e3
+    fs_var = 250e6
+    fs_block = fs_var
+    fs_inv_block = 1.0 / fs_var
+    ws.retrievalAddFreqShift(
+        covmat_block=[[fs_block]],
+        covmat_inv_block=[[fs_inv_block]],
+        df=fs_df,
+    )
+    n_extra = (poly_order + 1) + 1
+    ws.xa.value = np.append(ws.xa.value, np.zeros(n_extra))
+    now = datetime.now()
+    print(f"-- {now}: Successfully set retrieval quantities in arts")
+
+
+def compute_checks(ws):
+    """touch unused fields, close the retrieval definition and run the checks."""
+
+    ws.Touch(ws.rtp_mag)  # magnetic field
+    ws.Touch(ws.rtp_los)  # line of sight
+    ws.Touch(ws.rtp_pressure)  # pressure
+    ws.Touch(ws.rtp_temperature)  # temperature
+    ws.Touch(ws.rtp_nlte)  # NLTE temperature ratio
+    ws.Touch(ws.rtp_vmr)  # volume mixing ratio
+
+    ws.Touch(ws.wind_u_field)
+    ws.Touch(ws.wind_v_field)
+    ws.Touch(ws.wind_w_field)
+    ws.Touch(ws.time)
+    ws.cloudboxOff()
+
+    # close the retrieval definition and prepare the related WSVs
+    ws.retrievalDefClose()
+
+    # compute checks
+    ws.propmat_clearsky_agendaAuto()
+    ws.atmgeom_checkedCalc()
+    ws.lbl_checkedCalc()
+    ws.atmfields_checkedCalc()
+    ws.cloudbox_checkedCalc()
+    ws.sensor_checkedCalc()
+    ws.propmat_clearsky_agenda_checkedCalc()
+
+    now = datetime.now()
+    print(f"-- {now}: Successfully computed checks in arts")
+
+
+def compute_post_oem(ws):
+    ws.avkCalc()
+    ws.covmat_ssCalc()
+    ws.covmat_soCalc()
+    ws.retrievalErrorsExtract()
+    ws.x2artsSensor()
+
+
+def run_OEM(ws, meas: Measurement):
+    now = datetime.now()
+    print(f"-- {now}: Starting retrieval on {meas.source}")
+    ws.OEM(
+        method="lm",
+        lm_ga_settings=[8, 2, 2, 512, 1, 99],
+        display_progress=1,
+        max_iter=20,
+        verbosity=10,
+    )
+    res = ws.y.value - ws.yf.value
+    print(min(res), max(res))
+    now = datetime.now()
+    print(f"-- {now}: Done.")
+
+
+def first_pass_wrapper(fp):
+    obj = WaspamReader(fp=fp)
+    data = obj.load_data()
+
+    meas = Measurement(
+        data=data,
+        lines_cat="/home/ric/.cache/arts/arts-cat-data-2.6.18/lines/",
+        atm_cat="/home/ric/.cache/arts/arts-xml-data-2.6.18/planets/Earth/Fascod/subarctic-winter/subarctic-winter",
+        source=fp,
+    )
+    ws = pyarts.Workspace()
+    set_defaults(ws, meas)
+    set_atmosphere(ws, meas)
+    set_measurement(ws, meas)
+    set_sensor(ws, meas)
+    set_retrieval_quantities(ws, meas)
+    compute_checks(ws)
+    run_OEM(ws, meas)
+    compute_post_oem(ws)
+    return ws
+
+
+def save_retrieval(ws, fn):
+    n = len(ws.p_grid.value)
+    avk = ws.avk.value[0:n, 0:n]
+    q = ws.x.value[0:n]
+    qa = ws.xa.value[0:n]
+    ds = xr.Dataset(
+        data_vars={
+            "y": (["f"], ws.y.value),
+            "yf": (["f"], ws.yf.value),
+            "yb": (["f"], ws.y_baseline.value),
+            "avk": (["p", "p"], avk),
+            "q": (["p"], q),
+            "qa": (["p"], qa),
+        },
+        coords={
+            "f": ws.f_backend.value,
+            "p": ws.p_grid.value,
+        },
+    )
+    ds.to_netcdf(fn)
